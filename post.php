20377c42649fb8519f20167e55a3d76e75052649
Vitor André Gehrke <vitor.gehrke@gmail.com>
2024-05-13 21:06:38 -0300

    Mudanças no Post
    Incluído nome do editor e data de publicação + nome completo nos comentários

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php
-  include_once("templates/header.php");
+include_once("templates/header.php");
 
-  if(isset($_GET['id'])) {
+if (isset($_GET['id'])) {
     $postId = $_GET['id'];
 
     // Endpoint da API
@@ -9,56 +9,62 @@
 
     // Fazendo solicitação HTTP para obter os dados da postagem
     $response = file_get_contents($api_url);
-    
+
     if ($response !== false) {
-      $post = json_decode($response, true);
+        $post = json_decode($response, true);
 
-      // Verifica se a decodificação JSON foi bem-sucedida
-      if ($post !== null) {
-?>
+        // Verifica se a decodificação JSON foi bem-sucedida
+        if ($post !== null) {
+            ?>
 
-<main id="post-container">
-  <div class="content-container">
-    <h1 id="main-title"><?= $post['titulo'] ?></h1>
-    <p id="post-description"><?= $post['texto'] ?></p>
-    <?php if ($post['imagem'] !== null): ?>
-      <div class="img-container">
-        <img src="<?= $post['imagem'] ?>" alt="<?= $post['titulo'] ?>">
-      </div>
-    <?php endif; ?>
-    <?php if ($post['video'] !== null): ?>
-      <div class="video-container">
-        <video controls>
-          <source src="<?= $post['video'] ?>" type="video/mp4">
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    <?php endif; ?>
-  </div>
-  <aside id="nav-container">
-    <h3 id="categories-title">Categoria</h3>
-    <ul id="categories-list">
-      <li><a href="#"><?= $post['categoria'] ?></a></li>
-    </ul>
-    <h3 id="comments-title">Comentários</h3>
-    <ul id="comment-list">
-      <?php foreach($post['comentarios'] as $comment): ?>
-        <li>
-          <p><?= $comment['texto'] ?></p>
-          <p>Por: <?= $comment['usuario']['nome'] ?></p>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </aside>
-</main>
+            <main id="post-container">
+                <div class="content-container">
+                    <h1 id="main-title"><?= $post['titulo'] ?></h1>
+                    <p><strong>Publicado
+                            por:</strong> <?= $post['editor']['nome'] ?> <?= $post['editor']['sobrenome'] ?></p>
+                    <p><strong>Data de Publicação:</strong> <?= date('d/m/Y', strtotime($post['data'])) ?></p>
+                    <p id="post-description"><?= $post['texto'] ?></p>
+                    <?php if ($post['imagem'] !== null): ?>
+                        <div class="img-container">
+                            <img src="<?= $post['imagem'] ?>" alt="<?= $post['titulo'] ?>">
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($post['video'] !== null): ?>
+                        <div class="video-container">
+                            <video controls>
+                                <source src="<?= $post['video'] ?>" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <aside id="nav-container">
+                    <h3 id="categories-title">Categoria</h3>
+                    <ul id="categories-list">
+                        <li><a href="#"><?= $post['categoria'] ?></a></li>
+                    </ul>
+                    <h3 id="comments-title">Comentários</h3>
+                    <ul id="comment-list">
+                        <?php foreach ($post['comentarios'] as $comment): ?>
+                            <li>
+                                <p>
+                                    <strong>Por:</strong> <?= $comment['usuario']['nome'] ?> <?= $comment['usuario']['sobrenome'] ?>
+                                <p><strong>Data:</strong> <?= date('d/m/Y', strtotime($comment['data'])) ?></p>
+                                <p><?= $comment['texto'] ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </aside>
+            </main>
 
-<?php
-      } else {
-        echo "Erro ao decodificar os dados da API.";
-      }
+
+            <?php
+        } else {
+            echo "Erro ao decodificar os dados da API.";
+        }
     } else {
-      echo "Erro ao fazer solicitação para a API.";
+        echo "Erro ao fazer solicitação para a API.";
     }
-  }
-  include_once("templates/footer.php");
+}
+include_once("templates/footer.php");
 ?>
